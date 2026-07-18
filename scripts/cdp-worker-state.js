685ae179ee6c843a927459bf67a478b04b779203
Thespaceblade <jason.charwin360@gmail.com>
2026-07-18 14:16:02 -0400

    Remove tracker sync and tutorial from main.
    Keep that work on feature/tracker-and-tutorial until it is ready to ship.

--- scripts/cdp-worker-state.js
+++
@@ -1,7 +0,0 @@
-(() => ({
-    href: globalThis.location?.href || '',
-    constructorName: globalThis.constructor?.name || '',
-    runtimeId: globalThis.chrome?.runtime?.id || '',
-    hasStorage: !!globalThis.chrome?.storage?.local,
-    chromeKeys: Object.keys(globalThis.chrome || {}).slice(0, 30),
-}))()
